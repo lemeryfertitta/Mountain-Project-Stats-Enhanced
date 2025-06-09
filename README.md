02371c7cc54a61f9656769313045dc729c386a7d
Luke Emery-Fertitta <lemeryfertitta@gmail.com>
2025-06-09 07:16:54 -0600

    Add sorting to ticks

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ![icon](media/icon-32.png) Mountain Project Stats Enhanced
 
-This is a simple Chrome extension that adds dates to the stars, grades, and todos on the stats page of any Mountain Project route and makes these tables sortable by any column. The sizing of the columns on large screens is slightly adjusted to accommodate this additional info.
+This is a simple Chrome extension that adds dates to the stars, grades, and todos on the stats page of any Mountain Project route. Additionally, this extension allows for all of the tables on the stats page to be sorted by any column.
 
 ![screenshot](media/screenshot.jpg)
 
@@ -14,9 +14,10 @@ Add the extension to your Chrome browser from the [extension page](https://chrom
 - Noticing trends in quality or difficulty over time, e.g. due to a climb breaking at some point
 - Determining recency of TODO list additions for partner finding
 - Quickly viewing the min/max grade or star ratings using the new sorting functionality
+- Grouping ticks alphabetically to see someone's history on a route if they use multiple ticks
 
 ## Known Issues
 
 - MP did not appear to record timestamps prior to certain dates in 2017 depending on the data, so any timestamps marked with that date are relabelled with e.g. "≤ Mar 2017" for simplicity. Those entries could be from any date Mar 2017 or earlier.
-- MP paginates stats pages with large numbers of entries. This makes the sorting feature fairly useless on paginated tables until the whole table has been loaded manually by scrolling and clicking "Show More" repeatedly.
+- MP paginates stats pages with large numbers of entries. The sorting feature will only consider the loaded entries, so routes with paginated tables will not be correctly sorted until the whole table has been loaded manually by scrolling and clicking "Show More" repeatedly.
 - Sorting by grades on routes which use multiple grading systems (i.e. 5.12c V6) will usually not work correctly, as the sorting function just picks the first grade it sees.
--- script.js
+++ script.js
@@ -1,201 +1,227 @@
-let statResponses = [{}, {}, {}];
-const UNKOWN_REQUEST_INDEX = -1;
-const STARS_INDEX = 0;
-const RATINGS_INDEX = 1;
-const TODOS_INDEX = 2;
-const TICKS_INDEX = 3;
-const EPOCHS = [
-  new Date("2017-10-06T00:00:00Z"),
-  new Date("2017-03-21T00:00:00Z"),
-  new Date("2017-03-29T00:00:00Z"),
-];
-
-function getRatingValue(statResponse) {
-  const ratings = [
-    "rockRating",
-    "boulderRating",
-    "iceRating",
-    "aidRating",
-    "mixedRating",
-    "snowRating",
-  ];
-  for (const rating of ratings) {
-    if (statResponse[rating]) {
-      return statResponse[rating];
-    }
-  }
-}
-
-function getResponseIndex(url) {
-  if (url.includes("stars")) {
-    return STARS_INDEX;
-  } else if (url.includes("ratings")) {
-    return RATINGS_INDEX;
-  } else if (url.includes("todos")) {
-    return TODOS_INDEX;
-  } else {
-    return UNKOWN_REQUEST_INDEX;
-  }
-}
-
-function getResponseIndexFromTrId(id) {
-  if (!id) {
-    return TODOS_INDEX;
-  } else if (id.includes("stars")) {
-    return STARS_INDEX;
-  } else if (id.includes("ratings")) {
-    return RATINGS_INDEX;
-  } else {
-    return UNKOWN_REQUEST_INDEX;
-  }
-}
-
 (function () {
-  const originalFetch = window.fetch;
-  window.fetch = async function (...args) {
-    const response = await originalFetch(...args);
-    const responseIndex = getResponseIndex(args[0]);
-    if (responseIndex != UNKOWN_REQUEST_INDEX) {
-      const responseJson = await response.clone().json();
-      if (!responseJson.data) {
-        return response;
-      }
-      for (const dataItem of responseJson.data) {
-        if (!dataItem.user || !dataItem.user.id) {
-          continue;
-        }
-        statResponses[responseIndex][dataItem.user.id] = dataItem;
-      }
-    }
-    return response;
-  };
-})();
-
-function sortTable(tableBody, ascending, sortBy) {
-  const rows = Array.from(tableBody.querySelectorAll("tr")).slice(1);
-  rows.sort((a, b) => {
-    if (sortBy === "Date") {
-      const dateA = new Date(a.querySelector("td:last-child div").textContent);
-      const dateB = new Date(b.querySelector("td:last-child div").textContent);
-      return ascending ? dateB - dateA : dateA - dateB;
-    } else if (sortBy === "Name") {
-      const nameA = a
-        .querySelector("td:first-child a")
-        .textContent.toLowerCase();
-      const nameB = b
-        .querySelector("td:first-child a")
-        .textContent.toLowerCase();
-      return ascending
-        ? nameA.localeCompare(nameB)
-        : nameB.localeCompare(nameA);
-    } else if (sortBy === "Stars") {
-      const scoreA = parseFloat(a.getAttribute("data-score")) || 0;
-      const scoreB = parseFloat(b.getAttribute("data-score")) || 0;
-      return ascending ? scoreA - scoreB : scoreB - scoreA;
-    } else if (sortBy === "Grade") {
-      const ratingA = parseFloat(a.getAttribute("data-rating")) || 0;
-      const ratingB = parseFloat(b.getAttribute("data-rating")) || 0;
-      return ascending ? ratingA - ratingB : ratingB - ratingA;
-    } else {
-      console.warn(`Unknown sortBy: ${sortBy}`);
-      return 0; // No sorting if unknown
-    }
-  });
-  rows.unshift(tableBody.querySelector("tr"));
-  tableBody.replaceChildren(...rows);
-}
-
-function addHeaderElements(statsTable, observer) {
-  const headers = [
-    ["Name", "Stars", "Date"],
-    ["Name", "Grade", "Date"],
-    ["Name", "Date"],
+  let statResponses = [{}, {}, {}];
+  const UNKOWN_REQUEST_INDEX = -1;
+  const STARS_INDEX = 0;
+  const RATINGS_INDEX = 1;
+  const TODOS_INDEX = 2;
+  const TICKS_INDEX = 3;
+  const EPOCHS = [
+    new Date("2017-10-06T00:00:00Z"),
+    new Date("2017-03-21T00:00:00Z"),
+    new Date("2017-03-29T00:00:00Z"),
   ];
-  for (let tableIndex = 0; tableIndex < headers.length; tableIndex++) {
-    const tr = document.createElement("tr");
-    tr.id = `table-header-${tableIndex}`;
-    for (const header of headers[tableIndex]) {
-      const td = document.createElement("td");
-      const strong = document.createElement("strong");
-      strong.textContent = `${header} ↕`;
-      td.appendChild(strong);
-      td.addEventListener("click", () => {
-        const ascending = !td.classList.contains("ascending");
-        td.classList.toggle("ascending", ascending);
-        observer.disconnect(); // Stop observing while sorting
-        const tableBody = statsTable.querySelectorAll("tbody")[tableIndex];
-        sortTable(tableBody, ascending, header);
-        observer.observe(statsTable, {
-          childList: true,
-          subtree: true,
-          attributes: false,
-        });
-      });
-      tr.appendChild(td);
-    }
-    const tableBody = statsTable.querySelectorAll("tbody")[tableIndex];
-    tableBody.insertBefore(tr, tableBody.firstChild);
-  }
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-  const statsTable = document.querySelector(".onx-stats-table");
-  const observer = new MutationObserver((mutationsList, observer) => {
-    for (const mutation of mutationsList) {
-      for (const node of mutation.addedNodes) {
-        const responseIndex = getResponseIndexFromTrId(node.id);
-        if (
-          node.nodeName === "TR" &&
-          responseIndex != UNKOWN_REQUEST_INDEX &&
-          !node.querySelector(".date-col")
-        ) {
-          // Add date column to stars, ratings, and todos entries as they are added
-          const td = document.createElement("td");
-          td.classList.add("date-col");
-          const div = document.createElement("div");
-          div.className = "small";
-          const userId = node.querySelector("a")?.href?.split("/").pop();
-          if (!userId) {
-            console.debug(node);
-            console.debug("No user ID found in node:", node);
+  function getRatingValue(statResponse) {
+    const ratings = [
+      "rockRating",
+      "boulderRating",
+      "iceRating",
+      "aidRating",
+      "mixedRating",
+      "snowRating",
+    ];
+    for (const rating of ratings) {
+      if (statResponse[rating]) {
+        return statResponse[rating];
+      }
+    }
+  }
+
+  function getResponseIndex(url) {
+    if (url.includes("stars")) {
+      return STARS_INDEX;
+    } else if (url.includes("ratings")) {
+      return RATINGS_INDEX;
+    } else if (url.includes("todos")) {
+      return TODOS_INDEX;
+    } else {
+      return UNKOWN_REQUEST_INDEX;
+    }
+  }
+
+  function getResponseIndexFromTrId(id) {
+    if (!id) {
+      return TODOS_INDEX;
+    } else if (id.includes("stars")) {
+      return STARS_INDEX;
+    } else if (id.includes("ratings")) {
+      return RATINGS_INDEX;
+    } else {
+      return UNKOWN_REQUEST_INDEX;
+    }
+  }
+
+  (function () {
+    const originalFetch = window.fetch;
+    window.fetch = async function (...args) {
+      const response = await originalFetch(...args);
+      const responseIndex = getResponseIndex(args[0]);
+      if (responseIndex != UNKOWN_REQUEST_INDEX) {
+        const responseJson = await response.clone().json();
+        if (!responseJson.data) {
+          return response;
+        }
+        for (const dataItem of responseJson.data) {
+          if (!dataItem.user || !dataItem.user.id) {
             continue;
           }
-          const statResponse = statResponses[responseIndex][userId];
-          const tsDate = new Date(statResponse.updatedAt);
-          const dateString = new Intl.DateTimeFormat("en-US", {
-            year: "numeric",
-            month: "short",
-          }).format(tsDate);
-          div.textContent =
-            tsDate < EPOCHS[responseIndex] ? `≤ ${dateString}` : dateString;
-          td.appendChild(div);
-          node.appendChild(td);
-          if (responseIndex === STARS_INDEX) {
-            node.setAttribute("data-score", statResponse.score);
-          } else if (responseIndex === RATINGS_INDEX) {
-            node.setAttribute("data-rating", getRatingValue(statResponse));
-          }
-        } else if (node.nodeName === "DIV" && node.classList.contains("row")) {
-          // Populate headers once the tables have been added
-          addHeaderElements(statsTable, observer);
-          // Adjust table sizing on larger screens
-          node.children[STARS_INDEX].classList.replace("col-lg-2", "col-lg-3");
-          node.children[RATINGS_INDEX].classList.replace(
-            "col-lg-2",
-            "col-lg-3"
-          );
-          node.children[TICKS_INDEX].classList.replace("col-lg-6", "col-lg-4");
+          statResponses[responseIndex][dataItem.user.id] = dataItem;
         }
       }
+      return response;
+    };
+  })();
+
+  function sortTable(tableBody, ascending, sortBy) {
+    const rows = Array.from(tableBody.querySelectorAll("tr")).slice(1);
+    rows.sort((a, b) => {
+      if (sortBy === "Date") {
+        const dateA = new Date(
+          a.querySelector("td:last-child div").textContent
+        );
+        const dateB = new Date(
+          b.querySelector("td:last-child div").textContent
+        );
+        return ascending ? dateB - dateA : dateA - dateB;
+      } else if (sortBy === "Name") {
+        // Name can be null for private ticks
+        const nameA = a
+          .querySelector("td:first-child a")
+          ?.textContent.toLowerCase();
+        const nameB = b
+          .querySelector("td:first-child a")
+          ?.textContent.toLowerCase();
+        if (nameA && nameB) {
+          return ascending
+            ? nameA.localeCompare(nameB)
+            : nameB.localeCompare(nameA);
+        } else if (nameA) {
+          return ascending ? -1 : 1;
+        } else if (nameB) {
+          return ascending ? 1 : -1;
+        }
+        return 0;
+      } else if (sortBy === "Stars") {
+        const scoreA = parseFloat(a.getAttribute("data-score")) || 0;
+        const scoreB = parseFloat(b.getAttribute("data-score")) || 0;
+        return ascending ? scoreA - scoreB : scoreB - scoreA;
+      } else if (sortBy === "Grade") {
+        const ratingA = parseFloat(a.getAttribute("data-rating")) || 0;
+        const ratingB = parseFloat(b.getAttribute("data-rating")) || 0;
+        return ascending ? ratingA - ratingB : ratingB - ratingA;
+      } else if (sortBy === "Date/Notes") {
+        // These dates are in the ticks table and are always in a strong tag
+        const dateA = new Date(a.querySelector("strong").textContent);
+        const dateB = new Date(b.querySelector("strong").textContent);
+        return ascending ? dateB - dateA : dateA - dateB;
+      } else {
+        return 0;
+      }
+    });
+    rows.unshift(tableBody.querySelector("tr"));
+    tableBody.replaceChildren(...rows);
+  }
+
+  function addHeaderElements(statsTable, observer) {
+    const headers = [
+      ["Name", "Stars", "Date"],
+      ["Name", "Grade", "Date"],
+      ["Name", "Date"],
+      ["Name", "Date/Notes"],
+    ];
+    for (let tableIndex = 0; tableIndex < headers.length; tableIndex++) {
+      const tr = document.createElement("tr");
+      tr.id = `table-header-${tableIndex}`;
+      for (const header of headers[tableIndex]) {
+        const td = document.createElement("td");
+        const strong = document.createElement("strong");
+        strong.textContent = `${header} ↕`;
+        td.appendChild(strong);
+        td.addEventListener("click", () => {
+          const ascending = !td.classList.contains("ascending");
+          td.classList.toggle("ascending", ascending);
+          observer.disconnect(); // Temporarily stop observing to ignore sorting changes
+          const tableBody = statsTable.querySelectorAll("tbody")[tableIndex];
+          sortTable(tableBody, ascending, header);
+          observer.observe(statsTable, {
+            childList: true,
+            subtree: true,
+            attributes: false,
+          });
+        });
+        tr.appendChild(td);
+      }
+      const tableBody = statsTable.querySelectorAll("tbody")[tableIndex];
+      tableBody.insertBefore(tr, tableBody.firstChild);
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const statsTable = document.querySelector(".onx-stats-table");
+    const observer = new MutationObserver((mutationsList, observer) => {
+      for (const mutation of mutationsList) {
+        for (const node of mutation.addedNodes) {
+          const responseIndex = getResponseIndexFromTrId(node.id);
+          if (
+            node.nodeName === "TR" &&
+            responseIndex != UNKOWN_REQUEST_INDEX &&
+            !node.querySelector(".date-col")
+          ) {
+            // Add date column to stars, ratings, and todos tables
+            const td = document.createElement("td");
+            td.classList.add("date-col");
+            const div = document.createElement("div");
+            div.className = "small";
+            const userId = node.querySelector("a")?.href?.split("/").pop();
+            if (!userId) {
+              continue;
+            }
+            const statResponse = statResponses[responseIndex][userId];
+            const tsDate = new Date(statResponse.updatedAt);
+            const dateString = new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+            }).format(tsDate);
+            div.textContent =
+              tsDate < EPOCHS[responseIndex] ? `≤ ${dateString}` : dateString;
+            td.appendChild(div);
+            node.appendChild(td);
+            if (responseIndex === STARS_INDEX) {
+              node.setAttribute("data-score", statResponse.score);
+            } else if (responseIndex === RATINGS_INDEX) {
+              node.setAttribute("data-rating", getRatingValue(statResponse));
+            }
+          } else if (
+            node.nodeName === "DIV" &&
+            node.classList.contains("row")
+          ) {
+            // Populate headers once the tables have been added
+            addHeaderElements(statsTable, observer);
+            // Adjust table sizing on larger screens
+            node.children[STARS_INDEX].classList.replace(
+              "col-lg-2",
+              "col-lg-3"
+            );
+            node.children[RATINGS_INDEX].classList.replace(
+              "col-lg-2",
+              "col-lg-3"
+            );
+            node.children[TICKS_INDEX].classList.replace(
+              "col-lg-6",
+              "col-lg-4"
+            );
+          }
+        }
+      }
+    });
+    if (statsTable) {
+      observer.observe(statsTable, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+      });
+    } else {
+      console.error("MP Stats Extended: stats table not found on route page.");
     }
   });
-  if (statsTable) {
-    observer.observe(statsTable, {
-      childList: true,
-      subtree: true,
-      attributes: false,
-    });
-  } else {
-    console.error("Stats table not found");
-  }
-});
+})();
